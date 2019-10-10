feb4b0410be84e924d4bc9ee3dc5aa01ca06dcaa
ruda <sikora.katarzyna.waw@gmail.com>
2019-10-10 19:43:38 +0200

    Create home page Add img

---
+++ static/js/main.js
@@ -0,0 +1,84 @@
+const cardsArray = [
+    {
+        name: 'penguin',
+        img: "/static/img/penguin.jpg"
+    },
+    {
+        name: 'bear',
+        img: "/static/img/bear.jpg"
+    },
+    {
+        name: 'bool',
+        img: "/static/img/bool.jpg"
+    },
+    {
+        name: 'buffalo',
+        img: "/static/img/buffalo.jpg"
+    },
+    {
+        name: 'cangaroo',
+        img: "/static/img/cangaroo.jpg"
+    },
+    {
+        name: 'cat',
+        img: "/static/img/cat.jpg"
+    },
+    {
+        name: 'cat2',
+        img: "/static/img/cat2.jpg"
+    },
+    {
+        name: 'deer',
+        img: "/static/img/deer.jpg"
+    },
+    {
+        name: 'dog',
+        img: "/static/img/dog.jpg"
+    },
+    {
+        name: 'elephant',
+        img: "/static/img/elephant.jpg"
+    },
+    {
+        name: 'giraffe',
+        img: "/static/img/giraffe.jpg"
+    },
+    {
+        name: 'hippo',
+        img: "/static/img/hippo.jpg"
+    },
+    {
+        name: 'hog',
+        img: "/static/img/hog.jpg"
+    },
+    {
+        name: 'panther',
+        img: "/static/img/panther.jpg"
+    },
+    {
+        name: 'rabbit',
+        img: "/static/img/rabbit.jpg"
+    },
+    {
+        name: 'racoon',
+        img: "/static/img/racoon.jpg"
+    },
+    {
+        name: 'rino',
+        img: "/static/img/rino.jpg"
+    },
+    {
+        name: 'sluggard',
+        img: "/static/img/sluggard.jpg"
+    },
+    {
+        name: 'tiger',
+        img: "/static/img/tiger.jpg"
+    },
+    {
+        name: 'wolf',
+        img: "/static/img/wolf.jpg"
+    },
+];
+
+
