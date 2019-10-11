e9690115cda674136201b1e5c69406169420dc32
ruda <sikora.katarzyna.waw@gmail.com>
2019-10-11 19:17:24 +0200

    Add function to select cards

--- static/js/main.js
+++ static/js/main.js
@@ -8,16 +8,16 @@ const cardsArray = [
         img: "/static/img/bear.jpg"
     },
     {
-        name: 'bool',
-        img: "/static/img/bool.jpg"
+        name: 'bull',
+        img: "/static/img/bull.jpg"
     },
     {
         name: 'buffalo',
         img: "/static/img/buffalo.jpg"
     },
     {
-        name: 'cangaroo',
-        img: "/static/img/cangaroo.jpg"
+        name: 'kangaroo',
+        img: "/static/img/kangaroo.jpg"
     },
     {
         name: 'cat',
@@ -60,12 +60,12 @@ const cardsArray = [
         img: "/static/img/rabbit.jpg"
     },
     {
-        name: 'racoon',
-        img: "/static/img/racoon.jpg"
+        name: 'raccoon',
+        img: "/static/img/raccoon.jpg"
     },
     {
-        name: 'rino',
-        img: "/static/img/rino.jpg"
+        name: 'rhino',
+        img: "/static/img/rhino.jpg"
     },
     {
         name: 'sluggard',
@@ -83,18 +83,77 @@ const cardsArray = [
 
 let newCardsArray = cardsArray.concat(cardsArray);
 
+newCardsArray.sort(function () {return 0.5 - Math.random()});
+
 let game = document.getElementById('game');
 
 let grid = document.createElement('div');
-
 grid.setAttribute('class', 'grid');
 game.appendChild(grid);
 
+let count = 0;
+
+let firstChoice = '';
+
+let secondChoice = '';
+
+let previousChoice = '';
+
 
 for (let cards of newCardsArray) {
+
     const card = document.createElement('div');
     card.classList.add('card');
     card.dataset.name = cards.name;
-    card.style.backgroundImage = `url(${cards.img})`;
+    card.addEventListener('click', clickHandler);
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    cardBody.style.backgroundImage = `url(${cards.img})`;
+
+    const cardTop = document.createElement('div');
+    cardTop.classList.add('card-top');
+    cardTop.style.backgroundImage = `url('/static/img/leaf1.jpg')`;
+
+    card.appendChild(cardTop);
+    card.appendChild(cardBody);
     grid.appendChild(card);
 }
+
+
+function clickHandler(event) {
+    count++;
+    if (event.target.parentElement.getAttribute('data-name') === previousChoice) {
+        return
+    }
+    if (count <=2) {
+        if (count === 1) {
+            firstChoice = event.target.parentElement.getAttribute('data-name');
+            console.log('first' + firstChoice)
+        }
+        if (count === 2) {
+            secondChoice = event.target.parentElement.getAttribute('data-name');
+            console.log('second'+ secondChoice)
+        }
+
+        console.log(event);
+        let body = event.target.parentNode.firstChild;
+        body.style.display = 'none';
+
+        let top = event.target.parentNode.lastChild;
+        top.style.display = 'flex';
+        top.classList.add('selected');
+
+    } else {
+        let body = event.target.parentNode.firstChild;
+        body.style.display = 'flex';
+
+        let top = event.target.parentNode.lastChild;
+        top.style.display = 'none';
+        top.classList.remove('selected');
+        count = 0;
+    }
+    previousChoice = event.target.parentElement.getAttribute('data-name');
+}
+
+
